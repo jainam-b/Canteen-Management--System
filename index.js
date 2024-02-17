3b1fb028256ba309ac76ebda5915da7ea1535cfe
jainam-b <jainambagrecha16@gmail.com>
2024-02-17 21:54:55 +0530

    (filter):functionality added to retrive order with status

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const orderRouter = require("./routes/order")
 const userRouter = require("./routes/user");
+const filterRouter = require("./routes/filter");
 const   JWT_SECRET   = process.env.JWT_SECRET
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 app.use("/order", orderRouter)
 app.use("/user", userRouter)
+app.use("/status", filterRouter)
 
 const PORT = 3001;
 app.listen(PORT, () => {
