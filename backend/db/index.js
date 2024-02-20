9bb20ff0fa38b7aa9ae650748885cc3ccea9c73f
jainam-b <jainambagrecha16@gmail.com>
2024-02-20 21:04:07 +0530

    backend and frontend

---
+++ backend/db/index.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+mongoose.connect(
+  process.env.DB_URL
+);
+ 
+// Admin schema {admin to login }
+const userSchema = new mongoose.Schema({
+  username: { type: String },
+  password: { type: String},
+  role: {
+    type: String,
+    enum: ["customer", "staff", "admin"],
+    default: "staff",
+  },
+  name: String,
+  email: String,
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+// storing  itemId from menu table and quantity
+const orderItemSchema = new mongoose.Schema({
+  itemId: { type: mongoose.Schema.Types.ObjectId, ref: "MenuItem" },
+  quantity: { type: Number, default: 1 },
+});
+// storing customer id ,item form the orderItemSchema and status
+const orderSchema = new mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+     
+  },
+  items: [orderItemSchema],
+  status: {
+    type: String,
+    enum: ["pending", "preparing", "ready", "completed"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+// menu items 
+const menuItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  category: { type: String, enum: ['appetizer', 'main course', 'dessert'], required: true },
+  available: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
+});
+const Menu= mongoose.model('MenuItem', menuItemSchema);
+const Order = mongoose.model("Order", orderSchema);
+const User = mongoose.model("User", userSchema);
+ 
+
+module.exports = {
+  User,
+  Order,
+  Menu,
+  
+};
---
+++ backend/routes/filter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { Router } = require("express");
+const router = Router();
+const { Order } = require("../db");
+const userMiddleware = require("../middlewares/user");
+
+router.get("/:status", async (req, res) => {
+    try {
+        const status=req.params.status
+           // console.log(status);
+        const pendingOrders = await Order.find({ status: status });
+        res.json(pendingOrders);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
