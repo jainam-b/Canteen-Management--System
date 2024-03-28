f51fefcab380f55890043b7f28d154b7a5eecaf0
jainam-b <jainambagrecha16@gmail.com>
2024-03-28 18:58:16 +0530

    login and signup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,45 @@
-import React from "react";
-import { BrowserRouter,Routes, Router, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Routes, Router, Route } from 'react-router-dom'
 // import AddItem from "./Components/AddItem";
 // import Charts from "./Components/Charts";
-import Layout from './Components/shared/Layout';
-import Dashboard from './Components/Dashboard';
-import "./App.css";
-import ProductPage from "./Components/Products";
-import Try from "./Components/Try";
-import OrderDetails from "./Components/OrderDetails";
-import TeamMembers from "./Components/Members"
-// import Login from "./Components/Login/Login";
+import Layout from './Components/shared/Layout'
+import Dashboard from './Components/Dashboard'
+import './App.css'
+import ProductPage from './Components/Products'
+import Try from './Components/Try'
+import OrderDetails from './Components/OrderDetails'
+import TeamMembers from './Components/Members'
+import Login from './Components/Login/Login'
 // import Signup from "./Components/Signup/Signup";
 // import { ModalProvider } from "./Components/Context/ModalContext";
 import AdminProfile from './Components/yourprofile'
-import { ProductProvider } from './Components/ViewDetailModal/DetailContext';
+import { ProductProvider } from './Components/ViewDetailModal/DetailContext'
+import { AuthProvider } from './Components/Login/AuthContext'
+import Signup from './Components/Signup/Signup'
 
+function App() {
+    return (
+        <ProductProvider>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="try" element={<Try />} />
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Dashboard />} />
+                            <Route path="orders" element={<OrderDetails />} />
+                            <Route path="products" element={<ProductPage />} />
+                            <Route path="members" element={<TeamMembers />} />
+                            <Route path="yourprofile" element={<AdminProfile />} />
 
-function App(){
-  return(
-    <ProductProvider>
-    <BrowserRouter>
-      <Routes>
-          <Route path='try' element={<Try/>} />
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Dashboard />} />
-          <Route path='orders' element={<OrderDetails  />} />
-          <Route path='products' element={<ProductPage  />} />
-          <Route path='members' element={<TeamMembers  />} />
-          <Route path='yourprofile' element={<AdminProfile  />} />
-
-        {/* <Route path='Login' element={<Login  />} />
-        <Route path='Signup' element={<Signup  />} /> */}
-        </Route>
-    </Routes>
-  </BrowserRouter>
-  </ProductProvider>
-  )
+                            <Route path="login" element={<Login />} />
+                            <Route path='signup' element={<Signup  />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
+        </ProductProvider>
+    )
 }
-    
-    
-
-
 
 //  <BrowserRouter>
 
@@ -48,8 +47,8 @@ function App(){
 //           <Route path="/add-item" element={<AddItem />}></Route>
 //           {/* <Route path="/order-details" element={<OrderDetails />}></Route> */}
 //           <Route path="/chart" element={<Charts />}></Route>
-           
+
 //         </Routes>
 //       </BrowserRouter>
 
-export default App;
+export default App
